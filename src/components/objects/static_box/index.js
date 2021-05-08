a4e0957c4ab2413b799e9e5537f4b6ddaf91f0be
Alex <alex.dipasupil@gmail.com>
2021-05-08 12:47:49 -0400

    first set of updates

---
+++ src/components/objects/static_box/index.js
@@ -0,0 +1,1 @@
+export { default as Static_Box } from './static_box.js';
--- src/components/scenes/SeedScene.js
+++ src/components/scenes/SeedScene.js
@@ -1,7 +1,9 @@
 import * as Dat from 'dat.gui';
 import { Scene, Color } from 'three';
-import { Flower, Land } from 'objects';
+import { Flower, Land, Static_Box } from 'objects';
 import { BasicLights } from 'lights';
+import * as THREE from 'three';
+import { static_box } from '../objects/static_box';
 
 class SeedScene extends Scene {
     constructor() {
@@ -11,21 +13,33 @@ class SeedScene extends Scene {
         // Init state
         this.state = {
             gui: new Dat.GUI(), // Create GUI for scene
-            rotationSpeed: 1,
             updateList: [],
+            numberOfBoxes: 5,
         };
 
         // Set background to a nice color
-        this.background = new Color(0x7ec0ee);
+        this.background = new Color(0x000000);
 
         // Add meshes to scene
-        const land = new Land();
-        const flower = new Flower(this);
+        
         const lights = new BasicLights();
-        this.add(land, flower, lights);
+
+        var total = this.state.numberOfBoxes
+        console.log(total)
+
+        for (let i = 0; i < total; i++) {
+            let color = 0xffffff;
+
+            let box = new Static_Box(color);
+            this.add(box)
+            box.position.set(i + i - 3, 0, 0)
+        }
+
+        this.add(lights);
 
         // Populate GUI
-        this.state.gui.add(this.state, 'rotationSpeed', -5, 5);
+        // this.state.gui.add(this.state, 'numberOfBoxes', 1, 5);
+
     }
 
     addToUpdateList(object) {
@@ -33,8 +47,7 @@ class SeedScene extends Scene {
     }
 
     update(timeStamp) {
-        const { rotationSpeed, updateList } = this.state;
-        this.rotation.y = (rotationSpeed * timeStamp) / 10000;
+        const {updateList } = this.state;
 
         // Call update for each object in the updateList
         for (const obj of updateList) {
